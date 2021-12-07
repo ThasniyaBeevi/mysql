b44a6be127d6c6567ce39cb3505a76119d3aa64e
Thasniya Beevi.T <thasniyabeevit@gmail.com>
2021-12-07 16:20:10 +0530

    mysql lab submission

---
+++ lab1_complaint_website_2.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2021 at 11:45 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_complaint_website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agent_login`
+--
+
+CREATE TABLE `agent_login` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint_registration`
+--
+
+CREATE TABLE `complaint_registration` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `complaint_nature` varchar(50) NOT NULL,
+  `complaint_details` varchar(1000) NOT NULL,
+  `image_file` blob NOT NULL,
+  `reg_date_time` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_login`
+--
+
+CREATE TABLE `employee_login` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_complaint`
+--
+
+CREATE TABLE `manage_complaint` (
+  `id` int(11) NOT NULL,
+  `complaint_id` int(11) NOT NULL,
+  `status` enum('Complete','Progress','Stuck') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_employee`
+--
+
+CREATE TABLE `manage_employee` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `complaint_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `ph_no` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agent_login`
+--
+ALTER TABLE `agent_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `complaint_registration`
+--
+ALTER TABLE `complaint_registration`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `employee_login`
+--
+ALTER TABLE `employee_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `manage_complaint`
+--
+ALTER TABLE `manage_complaint`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `complaint_id` (`complaint_id`);
+
+--
+-- Indexes for table `manage_employee`
+--
+ALTER TABLE `manage_employee`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`),
+  ADD KEY `complaint_id` (`complaint_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agent_login`
+--
+ALTER TABLE `agent_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaint_registration`
+--
+ALTER TABLE `complaint_registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee_login`
+--
+ALTER TABLE `employee_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `manage_complaint`
+--
+ALTER TABLE `manage_complaint`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `manage_employee`
+--
+ALTER TABLE `manage_employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `complaint_registration`
+--
+ALTER TABLE `complaint_registration`
+  ADD CONSTRAINT `complaint_registration_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `manage_complaint`
+--
+ALTER TABLE `manage_complaint`
+  ADD CONSTRAINT `manage_complaint_ibfk_1` FOREIGN KEY (`complaint_id`) REFERENCES `complaint_registration` (`id`);
+
+--
+-- Constraints for table `manage_employee`
+--
+ALTER TABLE `manage_employee`
+  ADD CONSTRAINT `manage_employee_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employee_login` (`id`),
+  ADD CONSTRAINT `manage_employee_ibfk_2` FOREIGN KEY (`complaint_id`) REFERENCES `complaint_registration` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
